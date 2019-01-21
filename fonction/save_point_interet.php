da2905e5f608d705ecdcc74e9c4c3c1358b107fe
Benoît <46492549+benoitrey@users.noreply.github.com>
2019-01-21 21:42:44 +0100

    Add files via upload

--- fonction/save_point_interet.php
+++ fonction/save_point_interet.php
@@ -21,32 +21,49 @@
   $num_point = pg_escape_string($_POST['zs_num_point_pi']);
   $id_categorie = pg_escape_string($_POST['zl_nom_pic']);
   $url = pg_escape_string($_POST['zs_url_pi']);
-  $photo = '';
   $description = pg_escape_string($_POST['zs_description_pi']);
 
-
-  // Vérification du format des données
-  // URL à tester ???
-  // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
-  if(!empty($_FILES['zs_photo_pi']['name']))
-  {
-      $fichier_dossier_dest = '../image/photo_pi/';
-      $res_upload = verif_upload_image($_FILES['zs_photo_pi'],$fichier_dossier_dest);
-      //upload ok
-      if ($res_upload[0]=='OK'){
-        echo $res_upload[1];
-        $photo = $res_upload[2];
-      }
-      //upload ko
-      else {
-        echo $res_upload[1];
-        return(0);
-      }
+  // Récupération de la photo actuelle du point
+  $photo_old='';
+  if (!empty($id_point)){
+    $sql='select photo_pi from point_interet where id_interet_pi = '.$id_point.';';
+    $rs=pg_exec($idc,$sql);
+    $ligne=pg_fetch_assoc($rs);
+    $photo_old=$ligne['photo_pi'];
   }
-}
+
+  $fichier_a_charger = 0;
+  $fichier_dossier_dest = '../image/photo_pi/';
+  $fichier_temp = '';
+  $photo_new='';
 
   $msg = '';
   $action = '';
+
+
+  // Vérification du format des données
+
+  // URL à tester ???
+
+  // Vérification du format du fichier à uploader
+  if(!empty($_FILES['zs_photo_up']['name'])){
+    $fichier_temp = $_FILES['zs_photo_up']['tmp_name'];
+    $res_verif = verif_upload_image($fichier_temp);
+      
+    //format ok
+    if ($res_verif[0]=='OK'){
+      $fichier_a_charger = 1;
+      // extension du fichier
+      $fichier_ext = substr($_FILES['zs_photo_up']['name'],strrpos($_FILES['zs_photo_up']['name'], '.')+1);
+    }
+    //format ko
+    else {
+      echo $res_verif[1];
+      return(0);
+    }
+  }
+
+
   // Récupération de l'action à réaliser selon le bouton exécuté
   //bouton création/modification
   if (isset ($_POST['bt_submit_CM'])){
@@ -57,41 +74,76 @@
     $action = $_POST['bt_submit_S'];
   }
 
+
   //Suppression
   if ($action=="Supprimer"){
-    // supprimer fichier image du serveur
-
+    // Suppression du point en base
     $sql='delete from point_interet where id_interet_pi = '.$id_point.';';
     $rs=pg_exec($idc,$sql);
     $msg = "Suppression ok";
+
+    //suppression photo du serveur
+    if (!empty($photo_old)){
+      unlink($photo_old);
+    }
   }
   else{
     //insertion
     if ($action=="Créer"){
-          // test si tous les champs obligatoires ont bien été renseigné
-          if (!empty($parcours) & !empty($num_point) & !empty($id_categorie)) {
-            $sql='insert into point_interet (id_parcours_pi,num_point_pi,id_categorie_pi,url_pi,photo_pi,description_pi) values('.$parcours.','.$num_point.','.$id_categorie.',\''.$url.'\',\''.$photo.'\',\''.$description.'\')';
-            print($sql);
-            $rs=pg_exec($idc,$sql);
-            $msg = "Insertion ok";
-          else {
-            $msg = "Tous les champs obligatoires doivent être saisis";
-          }
-    }
-    //update
-    else {
       // test si tous les champs obligatoires ont bien été renseigné
-      if (!empty($parcours) & !empty($num_point) & !empty($id_categorie) & !empty($id_point)){
-        $sql='update point_interet set id_parcours_pi = '.$parcours.', num_point_pi = '.$num_point.', id_categorie_pi = '.$id_categorie.', url_pi = \''.$url.'\', photo_pi = \''.$photo.'\', description_pi = \''.$description.'\' where id_interet_pi = '.$id_point.';';
-        print($sql);
+      if (!empty($parcours) & !empty($num_point) & !empty($id_categorie)) {
+        // Insertion du point en base
+        $sql='insert into point_interet (id_parcours_pi,num_point_pi,id_categorie_pi,url_pi,description_pi) values('.$parcours.','.$num_point.','.$id_categorie.',\''.$url.'\',\''.$description.'\') returning id_interet_pi';
         $rs=pg_exec($idc,$sql);
-        $msg = "Modification ok";
-      }
-      else {
-        $msg = "Tous les champs obligatoires doivent être saisis";
+        $msg = "Insertion ok";
+
+        $ligne=pg_fetch_assoc($rs);
+        $id_point=$ligne['id_interet_pi'];
+        $photo_new = $fichier_dossier_dest.$id_point.".".$fichier_ext;
+
+        // Copie du fichier sélectionné sur le serveur
+        if ($fichier_a_charger = 1 & !empty($photo_new)){
+          if(move_uploaded_file($fichier_temp, $photo_new)){
+            $msg=$msg." / "."Fichier uploadé";
+            $sql='update point_interet set photo_pi = \''.$photo_new.'\' where id_interet_pi = '.$id_point.';';
+            $rs=pg_exec($idc,$sql);
+          }
+          else $msg=$msg." / "."Le fichier n'a pas pu être uploadé";
+        }  
+        else {
+          $msg = "Tous les champs obligatoires doivent être saisis";
+        }
       }
     }
+  //update
+  else {
+    // test si tous les champs obligatoires ont bien été renseigné
+    if (!empty($parcours) & !empty($num_point) & !empty($id_categorie) & !empty($id_point)){
+      
+      //suppression ancienne photo du serveur
+      if ($fichier_a_charger == 1){
+        if (!empty($photo_old)){
+          unlink($photo_old);
+        }
+        // nouvelle photo
+        $photo_new = $fichier_dossier_dest.$id_point.".".$fichier_ext;
+      }
+
+      $sql='update point_interet set id_parcours_pi = '.$parcours.', num_point_pi = '.$num_point.', id_categorie_pi = '.$id_categorie.', url_pi = \''.$url.'\', photo_pi = \''.$photo_new.'\', description_pi = \''.$description.'\' where id_interet_pi = '.$id_point.';';
+      $rs=pg_exec($idc,$sql);
+      $msg = "Modification ok";
+
+      // Copie du fichier sélectionné sur le serveur
+      if ($fichier_a_charger == 1 && !empty($photo_new)){
+        if(move_uploaded_file($fichier_temp, $photo_new)){
+          $msg=$msg." / "."Fichier uploadé";
+        }
+        else $msg=$msg." / "."Le fichier n'a pas pu être uploadé";
+      }
+    }
+    else $msg = "Tous les champs obligatoires doivent être saisis";
   }
+}
 
   echo $msg;
 ?>
--- fonction/verif_upload_image.php
+++ fonction/verif_upload_image.php
@@ -1,10 +1,7 @@
 <?php
-  function verif_upload_image($fichier,$fichier_dossier_dest){
+  function verif_upload_image($fichier_temp){
     $msg='';
     $res='KO';
-    // on récupère les infos du fichier à uploader
-    $fichier_temp = $fichier['tmp_name'];
-    $fichier_nom = $fichier['name'];
 
     // on défini les dimensions et le type du fichier
     list($fichier_larg, $fichier_haut, $fichier_type, $fichier_attr)=getimagesize($fichier_temp);
@@ -13,14 +10,6 @@
     $fichier_poids_max = 500000;
     $fichier_h_max = 2448;
     $fichier_l_max = 3264;
-
-    // extension du fichier
-    $fichier_ext = substr($fichier_nom,strrpos( $fichier_nom, '.')+1);
-
-    // on renomme le fichier
-    $fichier_date = date("ymdhis");
-    $fichier_n_nom = $fichier_dossier_dest.$fichier_date.".".$fichier_ext;
-    //$fichier_n_nom = $prefix_nom."_".$fichier_nom.".".$fichier_ext;
 
     // on vérifie s'il y a bien un fichier à uploader
     if (!empty($fichier_temp) && is_uploaded_file($fichier_temp))
@@ -45,6 +34,6 @@
     }
     else $msg="Pas de fichier à uploader<br />";
 
-    return array($res,$msg,$fichier_temp,$fichier_n_nom);
+    return array($res,$msg);
   }
 ?>
