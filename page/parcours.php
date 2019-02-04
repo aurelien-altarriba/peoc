c50c7409a93f73bd4d7c5987cf8cfd8ffabef049
Neilerua <aurelien.altarriba@gmail.com>
2019-02-04 17:18:14 +0100

    Récupération données parcours

--- page/parcours.php
+++ page/parcours.php
@@ -48,12 +48,37 @@
 			} catch(Exception $e) {
 				echo('erreur : '. $e);
 			}
+
+			// On récupère les données relatives au parcours
+			try {
+				$res_rel = pg_query_params($bdd,
+				 'SELECT nom_ne, nom_d, nom_m, prenom_m,
+				 	 (SELECT nom_pa
+						FROM pays
+						INNER JOIN membre ON id_membre_m = $1
+						WHERE id_pays_pa = id_pays_m)
+					 AS nom_pa
+					FROM parcours
+					INNER JOIN niveau_equestre ON id_niveau_p = id_niveau_ne
+					INNER JOIN departement ON id_departement_p = id_departement_d
+					INNER JOIN membre ON id_membre_p = id_membre_m
+					WHERE id_parcours_p = $1',
+
+					array($id_parcours));
+				$parcours_rel = pg_fetch_all($res_rel)[0];
+			} catch(Exception $e) {
+				echo('erreur : '. $e);
+			}
 		?>
 
 		<div id="contenu">
 			<div id="colonneGauche">
 				<h3><?php echo($parcours['nom_p']) ?></h3>
-				<div>En autonomie : <?php echo $parcours['autonomie_p'] ? '<b>OUI</b>' : '<b>NON</b>' ?></div>
+				<div>Créé le <?php echo date_format(date_create($parcours['dt_publication_p']), 'd/m/Y'); ?> par <?php echo $parcours_rel['prenom_m'] .' '. $parcours_rel['nom_m']; ?> (<?php echo $parcours_rel['nom_pa']; ?>)</div>
+				<div>En autonomie : <b><?php echo $parcours['autonomie_p'] ? 'OUI' : 'NON';?></b></div>
+				<div>Niveau : <b><?php echo $parcours_rel['nom_ne']; ?></b></div>
+				<div>Département : <b><?php echo $parcours_rel['nom_d']; ?> (<?php echo $parcours['id_departement_p']; ?>)</b></div>
+				<div><?php echo $parcours['description_p']; ?></div>
 			</div>
 
 			<div id="colonneDroite">
