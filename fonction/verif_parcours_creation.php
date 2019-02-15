83717c07af25f638c3bf77da3920dbcdeb5a0d7f
FORSTER FRANK <frank.forster@iutstid.univ-perp.fr>
2019-02-15 16:18:46 +0100

    édition des parcours en cours

---
+++ fonction/verif_parcours_creation.php
@@ -0,0 +1,77 @@
+<?php
+
+session_start();
+
+//Connexion BDD
+require_once('../include/connect.php');
+$idc = connect();
+
+// Création des variables recupérant les données du formulaire
+$nom_p = pg_escape_string($_POST['zs_nom_p']);
+$description_p = pg_escape_string($_POST['zs_description_p']);
+$id_niveau_p = pg_escape_string($_POST['zl_id_niveau_ne']);
+$id_departement_p = pg_escape_string($_POST['zl_id_departement_p']);
+$autonomie_p = pg_escape_string($_POST['autonomie_p']);
+$visible_p = pg_escape_string($_POST['visible_p']);
+
+
+//date du jour
+$t=time();
+$td=getdate($t);
+$today= '\''.$td['year'].'-'.$td['mon'].'-'.$td['mday'].'\'';
+
+// récupération du type de membre : centre_equestre, cavalier, NULL
+$type = $_SESSION['membre']['type']['0'];
+
+// récupération de l'id du membre connecté
+if (isset($_SESSION['membre']['id'])){
+  $id_membre = $_SESSION['membre']['id'];
+}
+else {
+  $id_membre = '';
+}
+
+// récupération de l'id du centre équestre connecté
+if (isset($_SESSION['membre']['ce']['id_centre_ce'])){
+  $id_centre_p = $_SESSION['membre']['ce']['id_centre_ce'];
+}
+else {
+  $id_centre_p = '';
+}
+
+// récupération le l'id du membre qui à créé le parcours sélectionné
+$rs = pg_query($idc,$sql = "SELECT id_membre_p
+FROM parcours
+WHERE id_parcours_p = $id_p");
+$id_membre_p_parcours_selection = pg_fetch_result($rs,0,0);
+
+// récupération le l'id du centre équestre qui à créé le parcours sélectionné
+$rs = pg_query($idc,$sql = "SELECT id_centre_p
+FROM parcours
+WHERE id_parcours_p = $id_p");
+$id_centre_p_parcours_selection = pg_fetch_result($rs,0,0);
+
+// vérifie si l'utilisateur est un cavalier pour pouvoir créer un parcours
+if ($type=='cavalier'){
+  //Insertion du parcours par un cavalier, id_membre_p est renseigné
+  $sql='INSERT INTO parcours(nom_p,description_p,id_niveau_p,id_departement_p,autonomie_p,visible_p,dt_publication_p, id_membre_p)
+  VALUES(\''.$nom_p.'\', \''.$description_p.'\', \''.$id_niveau_p.'\', \''.$id_departement_p.'\',\''.$autonomie_p.'\',\''.$visible_p.'\', '.$today.', '.$id_membre.')';
+  $rs=pg_exec($idc,$sql);
+  echo 'OK';
+}
+
+// vérifie si l'utilisateur est un centre questre pour pouvoir créer un parcours
+else if ($type=='centre_equestre') {
+  //Insertion du parcours par un centre_equestre, $id_centre_p est renseigné
+  $sql='INSERT INTO parcours(nom_p,description_p,id_niveau_p,id_departement_p,autonomie_p,visible_p,dt_publication_p, id_centre_p)
+  VALUES(\''.$nom_p.'\', \''.$description_p.'\', \''.$id_niveau_p.'\', \''.$id_departement_p.'\',\''.$autonomie_p.'\',\''.$visible_p.'\', '.$today.', '.$id_centre_p.')';
+  $rs=pg_exec($idc,$sql);
+  echo 'OK';
+}
+
+else
+  // Si l'utilisateur veut créer un parcours et n'est ni un cavalier, ni un centre équestre, il reçoit un message d'erreur
+  echo 'Vous devez être un centre équestre ou un cavalier pour créer un parcours';
+}
+
+?>
--- form/parcours.php
+++ form/parcours.php
@@ -15,104 +15,104 @@
 
 <?php echo('<h2>'. $_SESSION['membre']['prenom_m'] .' '. $_SESSION['membre']['nom_m'] .'</h2>'); ?>
 
-<form method="post" action="../fonction/verif_parcours.php">
+<form>
 
-  <!-- Choix du parcours à modifier-->
-  <div>
-    <?php if(isset($id)) { ?>
+    <!-- Choix du parcours à modifier-->
+    <div>
+      <?php if(isset($id)) { ?>
 
-    <label>Parcours à modifier</label>
-    <select name="zl_id_p" id="zl_id_p" required>
-    <?php
+      <label>Parcours à modifier</label>
+      <select name="zl_id_p" id="zl_id_p" required>
+      <?php
 
-    $sql='SELECT nom_p, id_parcours_p
-          FROM parcours';
+      $sql='SELECT nom_p, id_parcours_p
+            FROM parcours';
 
-    $rs=pg_exec($idc,$sql);
+      $rs=pg_exec($idc,$sql);
 
-      while($ligne=pg_fetch_assoc($rs)){
-        print('<option value="'.$ligne['id_parcours_p'].'">'.$ligne['nom_p'].'</option>');
-      }
-    ?>
-    </select>
+        while($ligne=pg_fetch_assoc($rs)){
+          print('<option value="'.$ligne['id_parcours_p'].'">'.$ligne['nom_p'].'</option>');
+        }
+      ?>
+      </select>
+    <?php } ?>
+    </div>
+
+    <!-- Zone de saisie du nom du parcours -->
+    <div>
+      <label for="zs_nom_p">Nom du parcours </label> <input type="text" name="zs_nom_p" id="zs_nom_p" maxlength="50" required>
+    </div>
+
+    <!-- Zone de saisie de la description du parcours -->
+    <div>
+      <label for="zs_description_p">Description </label> <textarea name="zs_description_p" id="zs_description_p"></textarea>
+    </div>
+
+    <!-- Choix du niveau de difficulté du parcours -->
+    <div>
+      <label>Niveau de difficulté</label>
+      <select name="zl_id_niveau_ne" id="zl_id_niveau_ne" required>
+      <?php
+
+      $sql='SELECT nom_ne, id_niveau_ne
+            FROM niveau_equestre;';
+
+      $rs=pg_exec($idc,$sql);
+
+        while($ligne=pg_fetch_assoc($rs)){
+          print('<option value="'.$ligne['id_niveau_ne'].'">'.$ligne['nom_ne'].'</option>');
+
+        }
+      ?>
+      </select>
+    </div>
+
+    <!-- Choix du département du parcours -->
+    <div>
+      <label>Département</label>
+      <select name="zl_id_departement_p" id="zl_id_departement_p" required>
+      <?php
+
+      $sql='SELECT nom_d, id_departement_d
+            FROM departement;';
+
+      $rs=pg_exec($idc,$sql);
+
+        while($ligne=pg_fetch_assoc($rs)){
+          print('<option value="'.$ligne['id_departement_d'].'">'.$ligne['nom_d'].'</option>');
+        }
+      ?>
+      </select>
+    </div>
+
+    <!-- Le parcours est-il faisable en autonomie ou pas -->
+    <div>
+
+        <p>Parcours autonome <br/>
+        <input type="radio" name="autonomie_p" id="autonomie_p_oui" value="TRUE"/>  <label for="autonomie_p_oui">Oui </label> <br/>
+        <input type="radio" name="autonomie_p" id="autonomie_p_non" value="FALSE"/>  <label for="autonomie_p_non">Non </label>
+        </p>
+    </div>
+
+    <!-- Choix de rendre le parcours visible au public -->
+    <div>
+
+        <p>Rendre le parcours public <br/>
+        <input type="radio" name="visible_p" id="visible_p_oui" value="TRUE" ><label for="visible_p_oui">Oui</label> <br/>
+        <input type="radio" name="visible_p" id="visible_p_non" value="FALSE" ><label for="visible_p_non">Non</label>
+        </p>
+    </div>
+
+    <!-- Bouton d'enregistrement -->
+    <input type="submit" name="bt_submit_data" id="bt_submit_data" value="Enregistrer le parcours" />
+
+  <?php if(isset($id)) { ?>
+    <!-- Bouton de suppression du parcours -->
+    <input type="submit" name="bt_submit_suppression" id="bt_submit_suppression" value="Supprimer le parcours" />
   <?php } ?>
-  </div>
 
-  <!-- Zone de saisie du nom du parcours -->
-  <div>
-    <label for="zs_nom_p">Nom du parcours </label> <input type="text" name="zs_nom_p" id="zs_nom_p" maxlength="50" required>
-  </div>
+  </form>
 
-  <!-- Zone de saisie de la description du parcours -->
-  <div>
-    <label for="zs_description_p">Description </label> <textarea name="zs_description_p" id="zs_description_p"></textarea>
-  </div>
-
-  <!-- Choix du niveau de difficulté du parcours -->
-  <div>
-    <label>Niveau de difficulté</label>
-    <select name="zl_id_niveau_ne" id="zl_id_niveau_ne" required>
-    <?php
-
-    $sql='SELECT nom_ne, id_niveau_ne
-          FROM niveau_equestre;';
-
-    $rs=pg_exec($idc,$sql);
-
-      while($ligne=pg_fetch_assoc($rs)){
-        print('<option value="'.$ligne['id_niveau_ne'].'">'.$ligne['nom_ne'].'</option>');
-
-      }
-    ?>
-    </select>
-  </div>
-
-  <!-- Choix du département du parcours -->
-  <div>
-    <label>Département</label>
-    <select name="zl_id_departement_p" id="zl_id_departement_p" required>
-    <?php
-
-    $sql='SELECT nom_d, id_departement_d
-          FROM departement;';
-
-    $rs=pg_exec($idc,$sql);
-
-      while($ligne=pg_fetch_assoc($rs)){
-        print('<option value="'.$ligne['id_departement_d'].'">'.$ligne['nom_d'].'</option>');
-      }
-    ?>
-    </select>
-  </div>
-
-  <!-- Le parcours est-il faisable en autonomie ou pas -->
-  <div>
-
-      <p>Parcours autonome <br/>
-      <input type="radio" name="autonomie_p" id="autonomie_p_oui" value="TRUE"/>  <label for="autonomie_p_oui">Oui </label> <br/>
-      <input type="radio" name="autonomie_p" id="autonomie_p_non" value="FALSE"/>  <label for="autonomie_p_non">Non </label>
-      </p>
-  </div>
-
-  <!-- Choix de rendre le parcours visible au public -->
-  <div>
-
-      <p>Rendre le parcours public <br/>
-      <input type="radio" name="visible_p" id="visible_p_oui" value="TRUE" ><label for="visible_p_oui">Oui</label> <br/>
-      <input type="radio" name="visible_p" id="visible_p_non" value="FALSE" ><label for="visible_p_non">Non</label>
-      </p>
-  </div>
-
-  <!-- Bouton d'enregistrement -->
-  <input type="submit" name="bt_submit_data" id="bt_submit_data" value="Enregistrer le parcours" />
-
-<?php if(isset($id)) { ?>
-  <!-- Bouton de suppression du parcours -->
-  <input type="submit" name="bt_submit_suppression" id="bt_submit_suppression" value="Supprimer le parcours" />
-<?php } ?>
-
-</form>
-
-<form method="get" action="../page/troncon.php">
-<button type="submit">Créer un tronçon</button>
-</form>
+  <form method="get" action="../page/troncon.php">
+    <button type="submit">Créer un tronçon</button>
+  </form>
