4292c7a9d6f568cd0d0094975f7dea009b6a1495
REY BENOIT <benoit.rey@iutstid.univ-perp.fr>
2019-05-13 15:31:59 +0200

    zone allure developpement

---
+++ fonction/verif_zone_allure.php
@@ -0,0 +1,71 @@
+<?php
+  session_start();
+  ini_set('display_errors', 1);
+
+  //Include
+  require_once($_SERVER['DOCUMENT_ROOT'] .'/include/config.php');
+
+  //Connexion BDD
+  require_once($_SERVER['DOCUMENT_ROOT'] .'/include/connect.php');
+  $idc = connect();
+
+
+  //bouton création/modification
+  $action = $_POST['action'];
+
+  $erreur = '';
+
+  // chargement des zones allures
+  if ($action==0){
+    $id_parcours = $_POST['id_parcours'];
+
+    $res = pg_query_params($idc,
+      "SELECT id_zone_za_t, id_parcours_za,id_type_za,
+        ST_AsGeoJSON(ST_Transform(geom_za,".$CF['srid']."))
+       FROM zone_allure
+       WHERE id_parcours_za = $1",
+     array($id_parcours));
+
+    $liste_za = pg_fetch_all($res);
+
+  	// On renvoie le résultat dans un tableau encodé en JSON
+    echo(json_encode($liste_za));
+  }
+
+
+  // creation d'une zone allure
+  elseif ($action==1){
+
+    $id_parcours = $_POST['id_parcours'];
+    $id_type = $_POST['id_type'];
+    $point1 = explode(",",$_POST['point1']);
+    $point2 = explode(",",$_POST['point2']);
+    $dist = $CF['dist_allure'];
+
+    $sql='SELECT id_ret, za_ret, msg_ret FROM create_allure('.$id_parcours.','.$id_type.','.$point1[0].','.$point1[1].','.$point2[0].','.$point2[1].','.$dist.')';
+
+
+  }
+
+
+  // suppression d'une zone allure
+  elseif ($action==2 && isset ($_POST['id'])){
+    $id=$_POST['id'];
+    $sql='DELETE FROM zone_allure WHERE id_zone_za = '.$id.';';
+
+    try{
+      $rs=pg_exec($idc,$sql);
+    }
+    catch (Exception $e) {
+      echo $e->getMessage(),"\n";
+    };
+  }
+
+
+
+
+  // Si une erreur est apparue
+  if(isset($erreur)) {
+    echo($erreur);
+  }
+?>
